21908c98e0d259a09888d1cdea75da6fe2673c0b
Craziilate <craziilate@yahoo.com.mx>
2024-09-24 02:05:03 -0600

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,13 @@
+function vertexto() {
+    var mostrarcondiciones=document.getElementById("condiciones");
+    var ocultarbotonver=document.getElementById("ver")
+    mostrarcondiciones.style.display="block";
+    ocultarbotonver.style.display="none";
+
+}
+function ocultartexto() {
+    var ocultartexto=document.getElementById("condiciones");
+    var mostrarboton=document.getElementById("ver")
+    ocultartexto.style.display="none";
+    mostrarboton.style.display="block";
+}
